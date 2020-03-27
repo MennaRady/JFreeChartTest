29d3e84fc7d9001ca2ea4bd2eb96f4947313e9f5
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-03-27 06:31:57 +0200

    added tests for delete method

--- tests/org/jfree/data/ComparableObjectSeriesTest.java
+++ tests/org/jfree/data/ComparableObjectSeriesTest.java
@@ -67,4 +67,75 @@ public class ComparableObjectSeriesTest {
         series.update(2, null);
     }
 
+    @Test
+    /// This test is to make sure that the start index of the data is deleted
+    public void Delete_DeletingItemsFromStartToEndIndex_StartDeleted(){
+        series = new ComparableObjectSeries(key);
+        series.add(4, null);
+        series.add(0, null);
+        series.add(2, null);
+
+        int start = 0;
+        int end = 2;
+
+        series.delete(start, end);
+        int actual = series.indexOf(4);
+        assertTrue(actual < 0);
+    }
+
+    @Test
+    /// This test is to make sure that the end index of the data is deleted
+    public void Delete_DeletingItemsFromStartToEndIndex_EndDeleted(){
+        series = new ComparableObjectSeries(key);
+        series.add(4, null);
+        series.add(0, null);
+        series.add(2, null);
+
+        int start = 0;
+        int end = 2;
+
+        series.delete(start, end);
+        int actual = series.indexOf(2);
+        assertTrue(actual < 0);
+    }
+
+    @Test
+    /// This test is to make sure that the all the required data is deleted
+    public void Delete_DeletingItemsFromStartToEndIndex_AllDeleted(){
+        series = new ComparableObjectSeries(key);
+        series.add(4, null);
+        series.add(0, null);
+        series.add(2, null);
+
+        int start = 0;
+        int end = 2;
+        int actual_size = series.data.size() - (end - start) - 1;
+
+        series.delete(start, end);
+        assertEquals(actual_size, series.data.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void Delete_IndexExceedingTheSize_ExceptionThrown(){
+        series = new ComparableObjectSeries(key);
+        series.add(4, null);
+        series.add(0, null);
+        series.add(2, null);
+
+        int start = 0;
+        int end = 3;
+
+        series.delete(start, end);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void Delete_DeletingFromEmptySeries_ExceptionThrown(){
+        series = new ComparableObjectSeries(key);
+
+        int start = 0;
+        int end = 3;
+
+        series.delete(start, end);
+    }
+
 }
